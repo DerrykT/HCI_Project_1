5db742db4f6dd41f18ac3e1d85a485f415874431
DerrykAlt <taylordm@clarkson.edu>
2023-01-26 17:06:06 -0500

    Added in test image

--- take_photo.py
+++ take_photo.py
@@ -11,3 +11,18 @@ ret, frame = vid.read()
 
 filename = 'savedImage.jpg'
 cv2.imwrite(filename, frame)
+
+
+# camera_port = 0
+# ramp_frames = 30
+# camera = cv2.VideoCapture(camera_port)
+# def get_image():
+#  retval, im = camera.read()
+#  return im
+# for i in range(ramp_frames):
+#  temp = camera.read()
+#
+# camera_capture = get_image()
+# filename = "testImage.jpg"
+# cv2.imwrite(filename,camera_capture)
+# del(camera)
