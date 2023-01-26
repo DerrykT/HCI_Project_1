dc1b8ed3ffed7e9807dd6cf6ce8028d14aaeed24
DerrykAlt <taylordm@clarkson.edu>
2023-01-26 15:58:50 -0500

    ..

---
+++ main.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+import pyttsx3
+import cv2
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voices', 'voices[0].id')
+
+engine.say("Hello World")
+
+# Get user supplied values
+imagePath = ""
+cascPath = ""
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
