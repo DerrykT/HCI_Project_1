1a6c1051aab11af2c6c93588b400b1b398f3dd2e
Vxcaroda <davidvicaro@gmail.com>
2023-01-26 17:09:07 -0500

    fixed darkness in take_photo

--- take_photo.py
+++ take_photo.py
@@ -4,25 +4,16 @@ import cv2
 
 # This program takes and saves a selfie from your webcam
 # int the current directory.
+camera_port = 0
+ramp_frames = 30
+camera = cv2.VideoCapture(camera_port)
+def get_image():
+    retval, im = camera.read()
+    return im
+for i in range(ramp_frames):
+    temp = camera.read()
 
-vid = cv2.VideoCapture(0)
-
-ret, frame = vid.read()
-
-filename = 'savedImage.jpg'
-cv2.imwrite(filename, frame)
-
-
-# camera_port = 0
-# ramp_frames = 30
-# camera = cv2.VideoCapture(camera_port)
-# def get_image():
-#  retval, im = camera.read()
-#  return im
-# for i in range(ramp_frames):
-#  temp = camera.read()
-#
-# camera_capture = get_image()
-# filename = "testImage.jpg"
-# cv2.imwrite(filename,camera_capture)
-# del(camera)
+camera_capture = get_image()
+filename = "testImage.jpg"
+cv2.imwrite(filename,camera_capture)
+del(camera)
