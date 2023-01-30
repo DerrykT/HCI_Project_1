1e9599a31720fac7e912f12d3600119ab5a6170c
Vxcaroda <davidvicaro@gmail.com>
2023-01-30 16:07:59 -0500

    created the camera window to show a live camera feed, and put this in a new file

--- take_photo.py
+++ take_photo.py
@@ -7,13 +7,17 @@ import cv2
 camera_port = 0
 ramp_frames = 30
 camera = cv2.VideoCapture(camera_port)
+
 def get_image():
     retval, im = camera.read()
     return im
+
 for i in range(ramp_frames):
     temp = camera.read()
 
 camera_capture = get_image()
 filename = "testImage.jpg"
+
 cv2.imwrite(filename,camera_capture)
+
 del(camera)
